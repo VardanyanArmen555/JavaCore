1942195bd79dc9af6a7b1a66a12a527b37ed38f6
Armen Vardanyan <avardanyan308070@gmail.com>
2023-08-25 15:37:30 +0400

    homeWork

--- src/classwork/chapter3/HomeWork.java
+++ src/classwork/chapter3/HomeWork.java
@@ -2,24 +2,12 @@ package classwork.chapter3;
 
 public class HomeWork {
     public static void main(String[] args) {
-        for (int i = 1; i <= 36; i++) {
-            System.out.print("*");
-            if (i == 6){
-                System.out.println();
-            }
-            if (i == 6*2){
-                System.out.println();
-            }
-            if (i == 6*3){
-                System.out.println();
-            }
-            if (i == 6*4){
-                System.out.println();
-            }
-            if (i == 6*5){
-                System.out.println();
-            }
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("*");
 
+            }
+            System.out.println();
         }
     }
 }
