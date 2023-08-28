b940919ef3fc90fdcc61e89e10598ba46c7d9811
Armen Vardanyan <avardanyan308070@gmail.com>
2023-08-28 22:55:36 +0400

    ArrayExample

---
+++ src/classwork/chapter3/IntExample.java
@@ -0,0 +1,14 @@
+package classwork.chapter3;
+
+public class IntExample {
+    public static void main(String[] args) {
+//        int a= 5;
+//        String aStr = a+" ";
+//        System.out.println(aStr);
+
+        String aStr = "567";
+        int a = Integer.parseInt(aStr);
+        System.out.println(a * 2);
+
+    }
+}
---
+++ src/classwork/chapter3/NumbersArrayExample.java
@@ -0,0 +1,14 @@
+package classwork.chapter3;
+
+public class NumbersArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        System.out.println(numbers[0]);
+//        int lastindex = numbers.length - 1;
+//        System.out.println(numbers[lastindex]);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+
+        }
+    }
+}
