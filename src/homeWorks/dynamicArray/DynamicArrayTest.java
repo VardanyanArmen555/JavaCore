d0c7b9f27d524bcf224d5647ee5fb930e5877b03
Armen Vardanyan <avardanyan308070@gmail.com>
2023-09-23 21:11:26 +0400

    DynamicArray

---
+++ src/homeWorks/dynamicArray/DynamicArrayTest.java
@@ -0,0 +1,12 @@
+package homeWorks.dynamicArray;
+
+public class DynamicArrayTest {
+    public static void main(String[] args) {
+        DynamicArray dy = new DynamicArray();
+        dy.add(7);
+        dy.add(6);
+        dy.add(8);
+
+        dy.print();
+    }
+}
